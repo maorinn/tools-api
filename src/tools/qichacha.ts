119aa3383b0c59a9df6b0ad9aade63d964008271
maoring21 <maoring21@gmail.com>
2024-02-19 11:26:29 +0800

    debug

--- src/tools/qichacha.ts
+++ src/tools/qichacha.ts
@@ -52,6 +52,33 @@ export async function abnormalCheck(searchKey: string) {
 }
 
 /**
+ * 严重违法核查
+ */
+export async function illegalCheck(searchKey: string) {
+  let timespan = Math.round(Date.now() / 1000);
+  // @ts-ignore
+  let token = getToken(
+    process.env.QCC_KEY,
+    timespan,
+    process.env.QCC_SECRET_KEY
+  );
+  let url = `https://api.qichacha.com/SeriousIllegalCheck/GetList?key=${process.env.QCC_KEY}&searchKey=${searchKey}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    // @ts-ignore
+    headers: {
+      Token: token,
+      Timespan: timespan,
+    },
+  });
+  console.log(3333, response);
+
+  const data = await response.json();
+  const { Result } = data;
+  return Result.Data || [];
+}
+
+/**
  * 获取token
  * Md5(key+Timespan+SecretKey) 加密的32位大写字符串
  */
